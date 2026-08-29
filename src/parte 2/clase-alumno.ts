58a4a5bb291841dbc689c93dfd47dd679bb8be8d
Ignacio <ignacioramirezlabadie@gmail.com>
2026-08-29 19:20:43 -0300

    feat(parte 2): resolver ejercicios 8, 9 y 10

--- src/parte 2/clase-alumno.ts
+++ src/parte 2/clase-alumno.ts
@@ -13,7 +13,7 @@
 // EJERCICIO 10 - interface Materia
 // -----------------------------------------------------------------------------
 //TODO cambiar a type
-export interface Materia {
+export type Materia = {
     codigo: number;
     nombre: string;
     horas: number;
@@ -36,10 +36,16 @@ export class Alumno {
         nombre: string,
         apellido: string,
         edad: number,
-        email: string
+        email: string,
+        materias: Materia[]
     ) {
         // TODO (Ejercicio 8): asignar los atributos recibidos.
-        throw new Error("Implementar");
+        this.legajo = legajo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.email = email;
+        this.materias = materias;
     }
 
     // -------------------------------------------------------------------
@@ -48,12 +54,12 @@ export class Alumno {
 
     getNombreCompleto(): string {
         // TODO
-        throw new Error("Implementar");
+        return `${this.nombre} ${this.apellido}`;
     }
 
     esMayorDeEdad(): boolean {
         // TODO
-        throw new Error("Implementar");
+        return this.edad >= 18;
     }
 
     // -------------------------------------------------------------------
@@ -62,14 +68,17 @@ export class Alumno {
 
     getEdad(): number {
         // TODO
-        throw new Error("Implementar");
+        return this.edad;
     }
 
     setEdad(edad: number): void {
         // TODO: debe impedir edades inválidas.
         // edad < 0   -> throw new Error(...)
         // edad > 120 -> throw new Error(...)
-        throw new Error("Implementar");
+        if (edad < 0 || edad > 120) {
+            throw new Error("La edad debe estar entre 0 y 120");
+        }
+        this.edad = edad;
     }
 
     // -------------------------------------------------------------------
@@ -78,28 +87,33 @@ export class Alumno {
 
     agregarMateria(materia: Materia): void {
         // TODO
-        throw new Error("Implementar");
+        this.materias.push(materia);
     }
 
     quitarMateria(codigo: number): Materia | undefined {
         // TODO: quitar la materia con ese código y devolverla.
         // Si no está inscripto en ninguna con ese código, devolver undefined.
-        throw new Error("Implementar");
+        const indice = this.materias.findIndex(materia => materia.codigo === codigo);
+        if (indice === -1) {
+            return undefined;
+        }
+        const materiaEliminada = this.materias.splice(indice, 1);
+        return materiaEliminada[0];
     }
 
     estaInscripto(codigo: number): boolean {
         // TODO
-        throw new Error("Implementar");
+        return this.materias.some(materia => materia.codigo === codigo);
     }
 
     cantidadMaterias(): number {
         // TODO
-        throw new Error("Implementar");
+        return this.materias.length;
     }
 
     getMaterias(): Materia[] {
         // TODO: devolver las materias sin exponer el arreglo interno
         // (devolver una copia, no la referencia original).
-        throw new Error("Implementar");
+        return [...this.materias];
     }
 }
